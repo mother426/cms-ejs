3ff339d8a7b4b811353da82422c1bb1eb3740198
John Kim <johnkimmsterr@gmail.com>
2021-06-21 12:36:49 -0400

    home returning

--- app.js
+++ app.js
@@ -15,10 +15,12 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
+app.get('/', (req, res) => {
+  res.render('home');
+});
 
 
 
 app.listen(3000, function() {
-  console.log("Server listening port https://localhost:3000");
+  console.log("Server listening port http://localhost:3000");
 });
